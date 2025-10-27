642a0e9ca2fd7eb2760d08686d0f9d1df2489a77
KeenTurbo <turbokeem@gmail.com>
2025-10-27 22:19:27 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const UUID = process.env.UUID || 'da3d2343-ad65-43f1-a252-b5ec066fb0fc'; // 使�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'databricks.snippet.pp.ua';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNmEzOWExZTg5OGVlNmZmMjZiZDc5YzkyN2ZmOTc4ZTQiLCJ0IjoiMmY0YzNlMDAtMGM0YS00MDM2LWE0MzMtMjc0YjU1NWM0Y2U4IiwicyI6IlltVTVZakZtTkdVdE9EQTJaUzAwT1dReExXSTJNR1l0Tm1VNE5tVTNZbU5tWm1VNCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || '47.76.218.163';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const NAME = process.env.NAME || 'DataBricks';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
